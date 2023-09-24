061b48c48945511b5faa1557e52ae00c7c3bb410
ParkYeongSeo <74289372+Frog-Slayer@users.noreply.github.com>
2023-09-24 10:58:05 +0900

    [Platinum V] Title: 등산 마니아, Time: 140 ms, Memory: 31620 KB, Score: 42 point -BaekjoonHub

---
+++ 백준/Platinum/20188. 등산 마니아/등산 마니아.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAX_N = 300001;
+int N, x, y;
+long long ans;
+vector<vector<int>> adj;
+vector<vector<int>> child;
+
+int makeTree(int root, int parent){
+	int size = 1;
+	for (auto c : adj[root]){
+		if (c == parent) continue;
+		child[root].push_back(c);
+		int subSize = makeTree(c, root);
+		size += subSize;
+		ans += 1LL * subSize * (N - subSize) + subSize * (subSize - 1) / 2;
+	}
+	return size; 
+}
+
+int main(){
+	scanf("%d", &N);
+	adj.resize(N + 1);
+	child.resize(N + 1);
+	for (int i = 0; i < N - 1; i++){
+		scanf("%d%d", &x, &y);
+		adj[x].push_back(y);
+		adj[y].push_back(x);
+	}
+
+	makeTree(1, 0);
+
+	printf("%lld", ans);
+}

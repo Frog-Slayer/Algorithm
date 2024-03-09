945e29a794e586c6753a214aa5a0910623ede3a7
ParkYeongSeo <74289372+Frog-Slayer@users.noreply.github.com>
2024-03-09 23:06:31 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+
+	if (a > b) swap(a, b);
+	if (a > c) swap(a, c);
+	if (b > c) swap(b, c);
+	
+	printf("%d %d %d", a, b, c);
+}

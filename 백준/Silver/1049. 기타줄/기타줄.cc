4e96f59da56524a692cb4af814756cb3c5d478d0
ParkYeongSeo <74289372+Frog-Slayer@users.noreply.github.com>
2024-08-03 17:51:54 +0900

    [Silver IV] Title: 기타줄, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1049. 기타줄/기타줄.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+int min_pack = 1000, min_single = 1000;
+
+int main(){
+	scanf("%d%d", &N, &M);
+
+	for (int i = 0; i < M; i++) {
+		int pack, single;
+		scanf("%d%d", &pack, &single);
+
+		min_pack = min(min_pack, pack);
+		min_single = min(min_single, single);
+	}
+
+	if (min_single * 6 < min_pack) printf("%d", min_single * N); 
+	else printf("%d", min(min_pack * (N / 6) + min_single * (N % 6), min_pack * (N / 6 + 1)));
+}

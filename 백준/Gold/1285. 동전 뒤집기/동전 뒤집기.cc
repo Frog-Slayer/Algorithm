bc4d986c35e65138eb91ce76d96f60152919fabf
ParkYeongSeo <74289372+Frog-Slayer@users.noreply.github.com>
2024-08-05 00:21:14 +0900

    [Gold I] Title: 동전 뒤집기, Time: 388 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -2,39 +2,32 @@
 using namespace std;
 
 int N;
-char coins[20][21];
+char buffer[21];
+int as_bit[20];
 
 int main() {
 	scanf("%d", &N);
 
 	for (int i = 0; i < N; i++) {
-		scanf("%s", coins[i]);
+		scanf("%s", buffer);
+
+		for (int j = 0; j < N; j++) if (buffer[j] == 'H') as_bit[i] |= (1 << j);
 	}
 
 	int ans = 0;
 	for (int i = 1; i < (1 << N); i++) {
-		for (int j = 0; j < N; j++) {
-			if (i & (1 << j)) {
-				for (int k = 0; k < N; k++) coins[j][k] = coins[j][k] == 'T' ? 'H' : 'T';
-			}
-			else for (int k = 0; k < N; k++) coins[j][k] = coins[j][k];
-		}
+		for (int j = 0; j < N; j++) if (i & (1 << j)) as_bit[j] = ~as_bit[j]; 
 
 		int sum = 0;
 		for (int j = 0; j < N; j++) {
 			int cnt = 0;
-			for (int k = 0; k < N; k++) cnt += coins[k][j] == 'H';
+			for (int k = 0; k < N; k++) cnt += (as_bit[k]) & (1 << j) ? 1 : 0;
 			if (cnt > N / 2) sum += cnt;
 			else sum += (N - cnt);
 		}
-		ans = max(ans, sum);
 
-		for (int j = 0; j < N; j++) {
-			if (i & (1 << j)) {
-				for (int k = 0; k < N; k++) coins[j][k] = coins[j][k] == 'T' ? 'H' : 'T';
-			}
-			else for (int k = 0; k < N; k++) coins[j][k] = coins[j][k];
-		}
+		for (int j = 0; j < N; j++) if (i & (1 << j)) as_bit[j] = ~as_bit[j]; 
+		ans = max(ans, sum);
 	}
 
 	printf("%d", N * N - ans);

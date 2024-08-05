b360bff93f11abddf83cc7b2604ae12ee5da3faf
ParkYeongSeo <74289372+Frog-Slayer@users.noreply.github.com>
2024-08-05 11:30:34 +0900

    [Platinum V] Title: 부분배열 고르기, Time: 16 ms, Memory: 3988 KB -BaekjoonHub

---
+++ 백준/Platinum/2104. 부분배열 고르기/부분배열 고르기.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+typedef long long ll;
+
+const int MAX_N = 100000;
+
+int N, height;
+int arr[MAX_N];
+ll sum[MAX_N];
+stack<ll> stk;
+
+int main() {
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+
+		sum[i] = arr[i];
+
+		if (i) sum[i] += sum[i - 1];
+	}
+
+	int left, right, index;
+	ll ans;
+
+	ans = index = left = right = 0;
+
+	while (index < N) {
+		if (stk.empty() || arr[stk.top()] <= arr[index]) {
+			stk.push(index); 
+			index++;
+		}
+		else if (!stk.empty() && arr[stk.top()] > arr[index]) {
+			right = index - 1; height = arr[stk.top()];
+
+			stk.pop();
+			if (stk.empty()) left = 0; 
+			else left = stk.top() + 1;
+
+			ans = max(ans, height * (sum[right] - (left ? sum[left - 1] : 0)));
+		}
+	}
+	
+	while (!stk.empty()) {
+		right = N - 1;
+		height = arr[stk.top()];
+		stk.pop();
+
+		if (stk.empty()) left = 0;
+		else left = stk.top() + 1;
+
+		ans = max(ans, height * (sum[right] - (left ? sum[left - 1] : 0)));
+	}
+
+	printf("%lld\n", ans);
+}

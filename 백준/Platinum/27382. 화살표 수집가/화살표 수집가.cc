1f720d9a15e61fa5f501af97a33cb37724488527
ParkYeongSeo <74289372+Frog-Slayer@users.noreply.github.com>
2024-09-04 18:24:47 +0900

    [Platinum I] Title: 화살표 수집가, Time: 2360 ms, Memory: 2328 KB -BaekjoonHub

---
+++ 백준/Platinum/27382. 화살표 수집가/화살표 수집가.cc
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+using namespace std;
+#define all(v) v.begin(), v.end()
+typedef long long ll;
+typedef pair<ll, ll> Point;
+
+vector<Point> v;
+ll operator!(const Point &p) { return p.first * p.first + p.second * p.second; }
+ll operator*(Point &p1, Point &p2) { return p1.first * p2.first + p1.second * p2.second; }
+ll operator/(Point &p1, Point &p2) { return p1.first * p2.second - p1.second * p2.first; }
+
+struct Segtree {
+	int N;
+	vector<int> tree;
+
+	void resize(int _N) {
+		N = _N;
+		tree.resize(4 * N);
+	}
+
+	void clear() {
+		tree.clear();
+		tree.resize(4 * N);
+	}
+
+	void update(int node, int start, int end, int left, int right, int val) {
+		if (right < start || end < left) return;
+		if (left <= start && end <= right) {
+			tree[node] += val;
+			return;
+		}
+
+		int mid = (start + end) / 2;
+		update(node * 2, start, mid, left, right, val);
+		update(node * 2 + 1, mid + 1, end, left, right, val);
+		tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	}
+
+	void update(int idx, int val) {
+		update(1, 0, N - 1, idx, idx, val);
+	}
+
+	ll query(int node, int start, int end, int left, int right) {
+		if (right < start || end < left) return 0;
+		if (left <= start && end <= right) return tree[node];
+
+		int mid = (start + end) / 2;
+		return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
+	}
+
+	ll query(int left, int right) {
+		return query(1, 0, N - 1, left, right);
+	}
+} tree;
+
+ll ans;
+
+void solve(int idx) {
+	vector<Point> B;
+	Point &a = v[idx];
+
+	for (Point &p : v) {
+		if (p.first <= a.first && p.second >= a.second) continue;
+		B.emplace_back(p.first - a.first, p.second - a.second);
+	}
+
+	sort(all(B), [&](Point &p1, Point &p2) {
+			Point o(0, 0);
+			if ((p1 < o)^(p2 < o)) return p1 < p2; 
+			return p1/p2 > 0; 
+			});
+
+	int b = 0;
+	while (b < B.size() && B[b].first <= 0) b++;
+
+	int c = 0, d = b;
+
+	vector<pair<int, int>> range(B.size());
+
+	while (b < B.size() && B[b].second < 0) {
+		while (c < b && B[c] * B[b] <= 0) c++;
+		while (d < B.size() && B[d] * B[b] > 0) d++;
+		range[b++] = { c, d - 1 }; 
+	}
+
+	vector<pair<ll, int>> dist(B.size());
+
+	for (int i = 0; i < B.size(); i++) dist[i] = { !B[i], i };
+
+	sort(all(dist));
+	tree.clear();
+
+	for (int i = 0, j = 0; i < B.size(); i = j) {
+		while (j < B.size() && dist[i].first == dist[j].first) j++;
+
+		for (int k = i; k < j; k++) {
+			int b = dist[k].second;
+			if (B[b].first > 0 && B[b].second < 0) {
+				int l = range[b].first, r = range[b].second; 
+				ans += tree.query(l, b - 1) * tree.query(b + 1, r);
+			}
+		}
+
+		for (int k = i; k < j; k++) tree.update(dist[k].second, 1); 
+	}
+}
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		int x, y;
+		scanf("%d%d", &x, &y);
+		v.emplace_back(x, y);
+	}
+
+	tree.resize(N - 1);
+	for (int i = 0; i < N; i++) solve(i);
+
+	printf("%lld", ans);
+
+}

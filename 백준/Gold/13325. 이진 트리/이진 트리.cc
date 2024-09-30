430025ce079cf7d40eb3eb100550ac8a06fb9197
ParkYeongSeo <74289372+Frog-Slayer@users.noreply.github.com>
2024-09-30 11:51:02 +0900

    [Gold III] Title: 이진 트리, Time: 512 ms, Memory: 10216 KB -BaekjoonHub

---
+++ 백준/Gold/13325. 이진 트리/이진 트리.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define fastio cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false)
+
+const int MAX_K = 20;
+vector<int> weight(1 << (MAX_K + 1));
+int K, ans = 0;
+
+int dfs(int depth, int root) {
+	if (depth > K) return 0;
+
+	int left = dfs(depth + 1, root * 2), right = dfs(depth + 1, root * 2 + 1);
+
+	int mx = max(left, right);
+	ans += (mx - left) + (mx - right);
+	return weight[root] += mx;
+}
+
+int main(){
+	cin >> K;
+
+	for (int i = 2; i < (1 << (K + 1)); i++){
+		cin >> weight[i];
+		ans += weight[i];
+	}
+
+	int a = dfs(0, 1);
+
+	printf("%d", ans);
+
+}

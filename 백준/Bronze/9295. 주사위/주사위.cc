87d64e9fe55596998d3a23f52584678f07fc738b
ParkYeongSeo <74289372+Frog-Slayer@users.noreply.github.com>
2024-10-11 22:18:16 +0900

    [Bronze IV] Title: 주사위, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int T;
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++) {
+		int n, m;
+		scanf("%d%d", &n, &m);
+		printf("Case %d: %d\n", i + 1, n + m);
+	}
+}

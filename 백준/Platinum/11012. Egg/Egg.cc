9afd1d5b2334285b9feb180b2dde337a749dcc26
ParkYeongSeo <74289372+Frog-Slayer@users.noreply.github.com>
2024-11-24 09:09:48 +0900

    [Platinum II] Title: Egg, Time: 824 ms, Memory: 18872 KB -BaekjoonHub

--- 백준/Platinum/11012. Egg/Egg.cc
+++ 백준/Platinum/11012. Egg/Egg.cc
@@ -1,98 +1,150 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <map>
 using namespace std;
 
-const int MAX = 1e5;
-int tree[MAX * 4 + 4], lazy[MAX * 4 + 4];
+#define fastio cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false)
+#define all(v) v.begin(), v.end()
 
-void propagate(int node, int start, int end) {
-	if (lazy[node]) {
-		tree[node] += lazy[node];
+typedef long long ll;
 
-		if (start != end) {
-			lazy[node * 2] += lazy[node];
-			lazy[node * 2 + 1] += lazy[node];
-		}
+struct Node {
+	ll val;
+	int left, right;
+
+	Node() : val(0), left(-1), right(-1) {} 
+
+	Node(const Node &node) {
+		val = node.val;
+		left = node.left;
+		right = node.right;
+	}
+};
+
+struct PST{
+	int N;
+	vector<Node> nodes;
+	map<int, int> root;
+
+	PST(int N, int Q): N(N) { 
+		nodes.reserve(4 * N + 18 * Q);
+	}
+
+	int copy_node(const Node& node) {
+		nodes.emplace_back(node);
+		return nodes.size() - 1;
+	}
+
+	int make_node() {
+		nodes.emplace_back();
+		return nodes.size() - 1;
+	}
+
+	int init(int start, int end) {
+		int node = make_node();
+
+		if (start == end)  return node;
 	
-		lazy[node] = 0;
-	}
-}
+		int mid = (start + end) / 2;
+		nodes[node].left = init(start, mid);
+		nodes[node].right = init(mid + 1, end);
 
-void update(int node, int start, int end, int left, int right, int val){
-	propagate(node, start, end);
-
-	if (right < start || end < left) return;
-	if (left <= start && end <= right) {
-		lazy[node] += val;
-		propagate(node, start, end);
-		return;
+		return node;
 	}
 
-	int mid = (start + end) / 2;
-	update(node * 2, start, mid, left, right, val); 
-	update(node * 2 + 1, mid + 1, end, left, right, val);
-}
+	void init() {
+		root[0] = init(0, N - 1);
+	}	
 
-void update(int left, int right, int val){
-	update(1, 0, MAX, left, right, val);
-}
+	int update(int node, int start, int end, int idx) {
+		node = copy_node(nodes[node]);
 
-int query(int node, int start, int end, int left, int right){
-	propagate(node, start, end);
+		if (start == end) {
+			nodes[node].val++;
+			return node;
+		}
 
-	if (right < start || end < left) return 0;
-	if (left <= start && end <= right) return tree[node];
+		int mid = (start + end) / 2;
 
-	int mid = (start + end) / 2;
-	return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
-}
+		if (idx <= mid) nodes[node].left = update(nodes[node].left, start, mid, idx);
+		else nodes[node].right = update(nodes[node].right, mid + 1, end, idx);
+		
+		nodes[node].val = nodes[nodes[node].left].val + nodes[nodes[node].right].val;
 
-int query(int idx) {
-	return query(1, 0, MAX, idx, idx);
-}
+		return node;
+	}
 
-int main(){
+	void update(int version, int idx) {
+		root[version] = update(root.rbegin()->second, 0, N - 1, idx);
+	}
+
+	ll query(int node, int start, int end, int left, int right) {
+		if (node < 0 || right < start || end < left) return 0;
+		if (left <= start && end <= right) return nodes[node].val; 
+
+		int mid = (start + end) / 2;
+
+		return query(nodes[node].left, start, mid, left, right) + query(nodes[node].right, mid + 1, end, left, right);
+	}
+
+	ll query(int version, int left, int right) {
+		return query(root[version], 0, N - 1, left, right);
+	}
+};
+
+int main() {
+	fastio;
+
 	int T;
-	scanf("%d", &T);
+	cin >> T;
 
 	while (T--) {
-		vector<int> eggs[MAX + 1];
-		vector<pair<int, int>> v_open[MAX + 1], v_close[MAX + 1];
+		int N, M;
+		cin >> N >> M;
 
-		for (int i = 0; i < MAX * 4 + 4; i++) tree[i] = lazy[i] = 0;
+		vector<pair<int, int>> points(N);
+		vector<int> xx, yy;
 
-		int n, m;
-		scanf("%d%d", &n, &m);
+		for (int i = 0; i < N; i++) {
+			cin >> points[i].first >> points[i].second;
+			xx.emplace_back(points[i].first);
+			yy.emplace_back(points[i].second);
+		}
+		
+		sort(all(points));
 
-		for (int i = 0; i < n; i++) {
-			int x, y;
-			scanf("%d%d", &x, &y);
-			eggs[x].push_back(y);
+		auto compress = [](vector<int> &v) {
+			sort(all(v));
+			v.erase(unique(all(v)), v.end());
+		};
+
+		compress(xx); compress(yy);
+	
+		PST pst(100001, N);
+		pst.init();
+
+		for (int i = 0, j = 0; i < N; i = j) {
+			while (j < N && points[i].first == points[j].first) j++;
+
+			for (int k = i; k < j; k++) {
+				auto &[x, y] = points[k];
+				x = lower_bound(all(xx), x) - xx.begin();
+				pst.update(x, y);
+			}
 		}
 
-		for (int i = 0; i < m; i++) {
+		ll ans = 0;
+		for (int i = 0; i < M; i++) {
 			int l, r, b, t;
-			scanf("%d%d%d%d", &l, &r, &b, &t);
-			v_open[l].emplace_back(b, t);
-			v_close[r].emplace_back(b, t);
+			cin >> l >> r >> b >> t;
+
+			l = lower_bound(all(xx), l) - xx.begin() - 1;
+			r = upper_bound(all(xx), r) - xx.begin() - 1;
+
+			ans += pst.query(r, b, t) - (l >= 0 ? pst.query(l, b, t) : 0);
 		}
 
-		int ans = 0;
-
-		for (int i = 0; i <= MAX; i++) {
-			for (auto open : v_open[i]) {
-				update(open.first, open.second, 1);
-			}
-
-			for (int egg: eggs[i]) {
-				ans += query(egg);
-			}
-
-			for (auto close : v_close[i]) {
-				update(close.first, close.second, -1);
-			}
-		}
-
-		printf("%d\n", ans);
+		cout << ans << '\n';
 	}
 }
